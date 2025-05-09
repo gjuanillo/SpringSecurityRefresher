1cf2bfe444163b98efc9f56201a6477f52be1344
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-09 20:51:20 +0800

    Configure In Memory User Details

--- src/main/java/com/jeiyuen/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/jeiyuen/springsecurity/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,10 +20,19 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         // Setups the security config to have any request be authenticated
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        // Make the API Stateless
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         // Setups the security config to have a basic authentication
         // Passes default security configuration (login with predefined credentials/defined credentials on app.props)
         http.httpBasic(withDefaults());
         // Builds the HTTP Security config and returns the bean as a SecurityFilterChain type
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1").password("{noop}demo123").roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}demo123").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
