39e3c4e4a657cdd7d76bc871840c66bd2eab7cc8
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-09 21:23:36 +0800

    Configure security to have custom role based access

--- src/main/java/com/jeiyuen/springsecurity/controller/DemoController.java
+++ src/main/java/com/jeiyuen/springsecurity/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.jeiyuen.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,19 @@ public class DemoController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    // NOTE: Pre auth annotation cannot have more than 1 role
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello, User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Hello, Admin!";
     }
 
 }
