b77551144ff01f75432b2d172e23821c4a9ee5ca
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-09 16:10:42 +0800

    Initialize Spring Security

---
+++ src/main/java/com/jeiyuen/springsecurity/SpringSecurityRefreshersApplication.java
@@ -0,0 +1,13 @@
+package com.jeiyuen.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRefreshersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRefreshersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeiyuen/springsecurity/SpringSecurityRefreshersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeiyuen.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRefreshersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
