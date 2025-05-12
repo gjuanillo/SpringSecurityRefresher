707c3ca237dc2ecee10611605df5c3adc18de771
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-12 21:44:29 +0800

    Configure H2 Database

--- src/main/java/com/jeiyuen/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/jeiyuen/springsecurity/config/SecurityConfig.java
@@ -21,12 +21,20 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         // Setups the security config to have any request be authenticated
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests
+                // Custom authorization (Permit role/all based on url)
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest()
+                .authenticated());
         // Make the API Stateless
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         // Setups the security config to have a basic authentication
         // Passes default security configuration (login with predefined credentials/defined credentials on app.props)
         http.httpBasic(withDefaults());
+        // Allow iframes to be displayed but only with the same origin
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+        // Disable cross-site request forgery protection
+        http.csrf(csrf -> csrf.disable());
         // Builds the HTTP Security config and returns the bean as a SecurityFilterChain type
         return http.build();
     }
