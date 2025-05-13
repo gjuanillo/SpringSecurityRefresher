a831678256e8e41b4b46d641add08c218e3e5426
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-13 20:46:10 +0800

    Create JWT Authentication Entrypoint/Response

---
+++ src/main/java/com/jeiyuen/springsecurity/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,44 @@
+package com.jeiyuen.springsecurity.jwt;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        // Set response content type into JSON
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        // Set HTTP status to 401/Unauthorized 
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        // Map objects into JSON format
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("path", request.getServletPath());
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+
+}
